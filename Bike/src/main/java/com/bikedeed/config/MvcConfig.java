6f96a65a0cfea1d3234d6710d31bed6ac091ecbf
Jorge Spiropulo <jo5h0y4@L-SFF01DHTDD-M.local>
2018-10-17 11:23:12 -0700

    changing packages from bike to bikedeed

--- Bike/src/main/java/com/bikedeed/config/MvcConfig.java
+++ Bike/src/main/java/com/bikedeed/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.bike.config;
+package com.bikedeed.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
