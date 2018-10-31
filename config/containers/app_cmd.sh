f8d09bf774b32399640f18bda977a6e7fe3ccfe0
ken woodruff <kwoodruf@adobe.com>
2018-10-30 19:34:11 -0700

    Added docker support.  rails, unicorn and redis in 1 container, postgres in another container.

---
+++ config/containers/app_cmd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ 
+touch log/development.log
+> log/development.log # Clear out development log, otherwise it balloons
+> log/test.log # Clear out test log too
+redis-server&
+# Prefix `bundle` with `exec` so unicorn shuts down gracefully on SIGTERM (i.e. `docker stop`)
+exec bundle exec unicorn_rails -p 3001;
+exec bundle exec rerun --background --dir app,db,lib --pattern '{**/*.rb}' -- bundle exec sidekiq;
